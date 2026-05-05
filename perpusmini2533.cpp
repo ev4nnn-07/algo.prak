8cd8f2cd3edf58622f3078ac97feb1bd058513f0
Jae-eon01 <revand.khemaluddin39@sma.belajar.id>
2026-05-05 20:28:17 +0700

    Perbaikan error dan penambahan data

--- perpusmini2533.cpp
+++ perpusmini2533.cpp
@@ -3,8 +3,7 @@
 #include <string.h>
 
 #define MAX 100
-struct Buku
-{
+struct Buku {
     char kode[10];
     char judul[50];
     char penulis[50];
@@ -12,58 +11,62 @@ struct Buku
     int status;
 };
 
-struct Buku data[MAX] int jumlah = 0;
+struct Buku data[MAX];
+    int jumlah = 0;
 
-void simpanFile()
-{
+void simpanFile(){
     FILE *fp = fopen("data.dat", "wb");
     if (fp == NULL)
     {
         printf("Gagal membuka file!\n");
         return;
     }
-    fwrite(% jumalah, sizeof(int), 1, fp);
+    fwrite(& jumlah, sizeof(int), 1, fp);
     fwrite(data, sizeof(struct Buku), jumlah, fp);
     fclose(fp);
 }
 
-vodi loadFile()
-{
+void loadFile(){
     FILE *fp = fopen("data.dat", "rb");
     if (fp != NULL)
     {
-        fread(&jumalah, sizeof(int), 1, fp);
+        fread(&jumlah, sizeof(int), 1, fp);
         fread(data, sizeof(struct Buku), jumlah, fp);
         fclose(fp);
     }
 }
 
-void tambahBuku()
-{
+void tambahBuku(){
     printf("\n==== Tambah Buku ====\n");
+    int n;
+    printf("Berapa banyakk buku yang ditambah?");
+    scanf("%d", &n);
+    getchar();
+
+    for(int k = 0; k < n; k++){
     printf("Kode Buku    : ");
     scanf("%s", data[jumlah].kode);
     getchar();
 
     printf("Judul Buku   : ");
-    fget(data[jumlah].judul, sizeof(data[jumlah].judul), stdin);
-    data[jumlah].judul[strcpn(data[jumlah].judul, "\n")] = 0;
+    fgets(data[jumlah].judul, sizeof(data[jumlah].judul), stdin);
+    data[jumlah].judul[strcspn(data[jumlah].judul, "\n")] = 0;
 
     printf("Penulis      : ");
-    fget(data[jumlah].penulis, sizeof(data[jumlah].penulis), stdin);
-    data[jumlah].penulis[strcpn(data[jumlah].penulis, "\n")] = 0;
+    fgets(data[jumlah].penulis, sizeof(data[jumlah].penulis), stdin);
+    data[jumlah].penulis[strcspn(data[jumlah].penulis, "\n")] = 0;
 
     printf("Tahun Terbit :");
     scanf("%d", &data[jumlah].tahun);
 
     data[jumlah].status = 0;
     jumlah++;
+    }
     simpanFile();
     printf("Data berhasil ditambahkan!\n");
 }
 
-void tampilBuku()
-{
+void tampilBuku(){
     printf("\n=== Data Buku ===\n");
     if (jumlah == 0)
     {
@@ -76,25 +79,26 @@ void tampilBuku()
         printf("Kode     : %s\n", data[i].kode);
         printf("Judul    : %s\n", data[i].judul);
         printf("Penulis  : %s\n", data[i].penulis);
-        printf("Tahun    : %s\n", data[i].tahun);
+        printf("Tahun    : %d\n", data[i].tahun);
         printf("Status   : %s\n", data[i].status == 0 ? "Tersedia" : "Dipinjam");
     }
 }
 
-void cariBuku()
-{
+void cariBuku(){
     char cari[50];
     printf("\nMasukan judul buku: ");
     getchar();
-    fget(cari, sizeof(cari), stdin);
+    fgets(cari, sizeof(cari), stdin);
     cari[strcspn(cari, "\n")] = 0;
 
-    strcpy(data[jumlah].judul, cari);
     int i = 0;
-    while (strcmp(data[i].judul, cari) != 0)
+    for(i=0; i<jumlah;i++)
     {
-        i++;
+        if(strcmp(data[i]. judul, cari)==0)
+    {
+        break;
     }
+}
 
     if (i < jumlah)
     {
@@ -102,12 +106,12 @@ void cariBuku()
         printf("Kode     : %s\n", data[i].kode);
         printf("Judul    : %s\n", data[i].judul);
         printf("Penulis  : %s\n", data[i].penulis);
-        printf("Tahun    : %s\n", data[i].tahun);
+        printf("Tahun    : %d\n", data[i].tahun);
         printf("Status   : %s\n", data[i].status == 0 ? "Tersedia" : "Dipinjam");
     }
     else
     {
-        printf("buku tidak ditemukan!\n");
+        printf("Buku tidak ditemukan!\n");
     }
 }
 
@@ -115,20 +119,18 @@ void shellsort()
 {
     int gap, i, j;
     struct Buku temp;
-    for (gap = jumlah / 2; gap > 0; gap /= 2)
-    {
+    for (gap = jumlah / 2; gap > 0; gap /= 2){
         for (i = gap; i < jumlah; i++)
         {
             temp = data[i];
-            for (j = i; j >= gap && strcmp(data[j - gap].judul, temp.judul) > 0; j -= gap)
-            {
+            for (j = i; j >= gap && strcmp(data[j - gap].judul, temp.judul) > 0; j -= gap){
                 data[j] = data[j - gap];
             }
             data[j] = temp;
         }
     }
     simpanFile();
-    printf("data berhasil diurutkan!\n");
+    printf("Data berhasil diurutkan!\n");
 }
 
 void hapusBuku()
@@ -164,11 +166,10 @@ void hapusBuku()
 }
 
 // meminjam buku
-
 void pinjamanBuku()
 {
     char kode[10];
-    printf("\nmasukkan kode buku yang ingin di pinjam:");
+    printf("\nMasukkan kode buku yang ingin di pinjam:");
     scanf("%s", kode);
 
     int i, found = 0;
@@ -183,20 +184,15 @@ void pinjamanBuku()
 
     if (found)
     {
-        if (data[i].status == 0)
-        {
+        if (data[i].status == 0){
             data[i].status = 1;
             simpanFile();
-            printf("buku berhasil dipinjam!!\n");
+            printf("Buku berhasil dipinjam!!\n");
+        } else {
+            printf("Buku sedang dipinjam!!\n");
         }
-        else
-        {
-            printf("buku sedang dipinjam!!\n");
-        }
-    }
-    else
-    {
-        printf("buku tidak ditemukan1\n")
+    } else {
+        printf("Buku tidak ditemukan!\n");
     }
 }
 
@@ -205,7 +201,7 @@ void pinjamanBuku()
 void kembalikanbuku()
 {
     char kode[10];
-    printf("\nmasukkan kode buku yang ingin dikembalikan:");
+    printf("\nMasukkan kode buku yang ingin dikembalikan:");
     scanf("%s", kode);
 
     int i, found = 0;
@@ -224,21 +220,16 @@ void kembalikanbuku()
         {
             data[i].status = 0;
             simpanFile();
-            printf("buku berhasil dikembalikan!\n");
+            printf("Buku berhasil dikembalikan!\n");
+        } else {
+            printf("Buku tidak sedang dipinjam!\n");
         }
-        else
-        {
-            printf("buku tidak sedang dipinjam!!\n");
-        }
-        else
-        {
-            printf("buku tidak ditemukan!\n");
+    } else {
+            printf("Buku tidak ditemukan!\n");
         }
     }
-}
 
 // menu utamanya
-
 int main()
 {
     loadFile();
@@ -246,17 +237,18 @@ int main()
 
     do
     {
-        printtf("\n=== SISTEM PERPUSTAKAKAN MINI ===\n");
+        printf("\n=== SISTEM PERPUSTAKAKAN MINI ===\n");
         printf("1. Tambah Buku\n");
         printf("2. Tampil Buku\n");
-        printf("3. cari buku\n");
+        printf("3. Cari buku\n");
         printf("4. Urutkan Buku\n");
         printf("5. Hapus Buku\n");
         printf("6. Pinjam Buku\n");
         printf("7. Kembalikan Buku\n");
-        printf("8. kelur\n");
-        printf("pilih menu:   ");
+        printf("8. Keluar\n");
+        printf("Pilih menu:   ");
         scanf("%d", &pilihan);
+        getchar();
 
         switch (pilihan)
         {
@@ -285,7 +277,7 @@ int main()
             printf("Terima kasih!\n");
             break;
         default:
-            printf("pilihan tidak valid!\n");
+            printf("Pilihan tidak valid!\n");
         }
     } while (pilihan != 8);
 
